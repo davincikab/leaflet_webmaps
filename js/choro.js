8322d4fcfe733cfdca496696802d34dfdf35975e
Davincikab <davidnganganjeri079@gmail>
2020-05-13 18:09:44 +0300

    Updated the choropleth map view

--- js/choro.js
+++ js/choro.js
@@ -1,7 +1,8 @@
+var colorSection = document.getElementById('color-picker');
 // Creating map object
 var myMap = L.map("map", {
-    center: [34.0522, -118.2437],
-    zoom: 8
+    center: [38.70265930723801, -96.50390625000001],
+    zoom: 5
 });
 
 // Adding tile layer
@@ -14,6 +15,16 @@ var layer = L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}'
  });
 
  layer.addTo(myMap);
+var colorSchemes = {
+    scheme1: ['#ecda9a', '#efc47e' , '#f3ad6a' , '#f7945d', '#f97b57', '#f66356', '#ee4d5a'],
+    scheme2:['#ffc6c4', '#f4a3a8', '#e38191', '#cc607d', '#ad466c', '#8b3058', '#672044'],
+    scheme3:['#d3f2a3', '#97e196', '#6cc08b', '#4c9b82', '#217a79', '#105965', '#074050'],
+    scheme4:['#fcde9c', '#faa476', '#f0746e', '#e34f6f', '#dc3977', '#b9257a', '#7c1d6f'],
+    scheme5:['#d1eeea', '#a8dbd9', '#85c4c9', '#68abb8', '#4f90a6', '#3b738f', '#2a5674'],
+    scheme6:['#fef6b5', '#ffdd9a', '#ffc285', '#ffa679', '#fa8a76', '#f16d7a', '#e15383'],
+    scheme7:['#b0f2bc', '#89e8ac', '#67dba5', '#4cc8a3', '#38b2a3', '#2c98a0', '#257d98']
+};
+
 // Load in geojson data
 var geoData = "/data/gunDeath.geojson";
 
@@ -27,7 +38,7 @@ fetch(geoData)
     });
 
 // choropleth scales
-// EmrId [#d3f2a3, #97e196, #6cc08b, #4c9b82, #217a79, #105965, #074050]
+// EmrId['#d3f2a3, '#97e196, '#6cc08b, '#4c9b82, '#217a79, '#105965, '#074050]
 // Burg #ffc6c4,#f4a3a8,#e38191,#cc607d,#ad466c,#8b3058,#672044,
 // OrYl #ecda9a,#efc47e,#f3ad6a,#f7945d,#f97b57,#f66356,#ee4d5a
 // ["#ffffb2", "#b10026"], ['#006837',"#ffffb2"],
@@ -42,21 +53,113 @@ function createChoropleth(data){
         scale: ['#ffffff', '#ff0000'],
 
         // Number of breaks in step range
-        steps: 9,
+        steps: 7,
 
         // q for quartile, e for equidistant, k for k-means
         mode: "q",
         style: {
             // Border color
             color: "#ddd",
-            weight: 0.2,
+            weight: 0.5,
             fillOpacity: 0.7
         },
 
         // Binding a pop-up to each layer
         onEachFeature: function (feature, layer) {
-            layer.bindPopup("Zip Code: " + feature.properties.NAME + "<br>Death Count:<br>" +
-                "$" + feature.properties.NUMPOINTS);
+            var popupContent;
+            layer.on('click', e =>{
+                myMap.closePopup();
+                let fillColor = layer.options.fillColor;
+
+                popupContent = "<div style='background-color:" + fillColor + ";' class='text-center text-white py-1 h6 header'>"
+                        + feature.properties.NAME+
+                        "</div><div class='text-center pb-2'> <h5>"+ feature.properties.NUMPOINTS +"</h5>Gun Deaths" +
+                        "</div>";
+
+                layer.bindPopup(popupContent).openPopup();
+            });
+
+            
         }
     }).addTo(myMap);
+
+    console.log(geojson);
+}
+
+
+// click handler
+var previousScheme;
+var colorChoices = document.getElementsByClassName('color-section');
+for (const colorScheme of colorChoices) {
+    colorScheme.addEventListener('click', function(e){
+        
+        if(previousScheme){
+            previousScheme.classList.remove('active-color');
+        }
+
+        let id = this.getAttribute('id');
+        let color = colorSchemes[id];
+        changeMapColor(color);
+
+        previousScheme = document.getElementById(id);
+
+        this.classList.toggle('active-color');
+
+
+        // Update the backgroundColor
+        colorSection.style.backgroundImage = "linear-gradient(45deg, "+color+")";
+    });
+}
+
+
+function changeMapColor(colors){
+    let limits = geojson.options.limits;
+
+    geojson.eachLayer((layer) => {
+        layer.setStyle({'fillColor':getFillColor(layer.feature,limits, colors)});
+    });
+
+   
+}
+
+function getFillColor(feature, limits, colors){
+    let value = feature.properties.NUMPOINTS;
+
+    let limit = limits.find((lim,i) => {
+        if(value <= lim){
+            return lim;
+        }
+    });
+
+   
+    let limitIndex = limits.indexOf(limit);
+    fillColor = colors[limitIndex];
+
+    return fillColor;
+}
+
+
+// Change Opacity
+var opacityElement = document.getElementById('opacity-level');
+opacityElement.addEventListener('change', function(e){
+
+    let value = e.srcElement.value;
+
+    geojson.eachLayer(layer => {
+        layer.setStyle({
+            'fillOpacity':value
+        });
+    });
+});
+
+// change the number of classes
+
+
+class ColorPicker{
+    constructor(){
+
+    }
+
+    // getter and setters
+    
 }
--- js/index.js
+++ js/index.js
@@ -22,19 +22,35 @@ layer.addTo(map);
 var counties = L.geoJSON(null, {
     style: function (feature) {
         return {
-            fillColor: '#C2C8CC',
-            color: '#fff',
-            weight: 0.7,
-            fillOpacity: 0.5
+            fillColor: styleCounties(feature),
+            color: '#ddd',
+            weight: 0.5,
+            fillOpacity: 0.90
         }
     }
 });
+
+
+function styleCounties(feature){
+    let value = feature.properties.sec_fiel_2;
+    // #fcde9c, #faa476, #f0746e, #e34f6f, #dc3977, #b9257a, #7c1d6f
+    return value <= 0.4 ? '#fcde9c' : value <= 0.5 ? '#faa476' : value <= 0.6 ? '#f0746e' : value <= 0.7 ? '#e34f6f' :
+        value <= 0.8 ? '#dc3977' : value <= 0.9 ? ' #b9257a' : value <= 1.0 ? '#7c1d6f' : '#7c1d6f';
+
+    // #d3f2a3, #97e196, #6cc08b, #4c9b82, #217a79, #105965, #074050
+    return value <= 0.4 ? '#d3f2a3' : value <= 0.5 ? '#97e196' : value <= 0.6 ? '#6cc08b' : value <= 0.7 ? '#4c9b82' :
+        value <= 0.8 ? '#217a79' : value <= 0.9 ? '#105965' : value <= 1.0 ? '#074050' :'#074050';
+
+    // '#ecda9a', '#efc47e', '#f3ad6a', '#f7945d', '#f97b57', '#f66356', '#ee4d5a'
+    // return value <= 0.4 ? '#ecda9a' : value <= 0.6 ? '#f3ad6a' : value <= 0.8 ? '#f7945d' : value <= 1.0 ? '#f66356' : '#ee4d5a';
+}
 
 counties.addTo(map);
 // // Get data
 $.getJSON('data/transition.geojson')
     .done(function (data) {
         counties.addData(data);
+        counties.bringToBack();
     }).fail(function (error) {
         console.log(error);
 });
@@ -59,6 +75,7 @@ var pointLayer = L.geoJSON(null,{
     }
 });
 
+pointLayer.addTo(map);
 // Point Layer Radius Size
 function setRadius(feature){
     // Normalize the values
@@ -68,12 +85,14 @@ function setRadius(feature){
 var legend = L.control({position:'bottomright'});
 legend.onAdd = function creatLegend(map){
     let div = L.DomUtil.create('div', 'container legend');
-    let values = [8,10,12,,14,16,18,20,24];
+    let values = [8,10,12,14,16,18,20,24];
 
     div.innerHTML += "<h4 class='mb-5 text-white'>County GPI </h4>";
-    values.forEach(function(value){
-        div.innerHTML += "<div class='legend-info' style='height:" + value * 2 + "px; width:" +
-            value * 2 + "px;'><small class='h4 mt-5'> "+value/20+" <small></div>";
+    values.forEach(function(value, i){
+        let size = value * 2;
+
+        div.innerHTML += "<div class='legend-container'><div class='legend-info' style='height:" + size+ "px; width:" +
+            size+ "px;'></div><span style='margin-left:"+ 48/value+"px;'>" + value / 20 +"</span></div>";
         
     });
 
@@ -92,4 +111,12 @@ fetch('data/proportionalSymbol.geojson')
    })
    .catch(error=> console.log("Failed to load the data"));
 
-
+// zoom event listeners
+map.on('zoomend', function(e){
+    let zoom = map.getZoom();
+    if(zoom < 7){
+        map.removeLayer(pointLayer);
+    }else{
+        map.hasLayer(pointLayer)?'':map.addLayer(pointLayer);
+    }
+});
